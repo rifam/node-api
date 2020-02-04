15dfc9f0d11062c21514dde903ad97423a4a3620
“rifam” <vinicius.rifam@gmail.com>
2020-02-04 11:52:21 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+
+//iniciando o app
+const app = express();
+
+//iniciando o DB
+
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true },
+);
+
+
+requireDir('./src/models');
+
+
+//Rotas
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
