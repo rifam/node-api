260faac02cc0e7d4dbf8aa04cd119646a25873ad
“rifam” <vinicius.rifam@gmail.com>
2020-02-05 11:47:50 -0300

    Reestruturação de arquivos

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
+app.use(express.json());
 
 //iniciando o DB
 
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
-routes.get('/products', ProductController.index);
+routes.get ('/products', ProductController.index);
+routes.post('/products', ProductController.store);
 
  module.exports = routes; 
