b9fc08f6f0ebb80b28f257ec43354cb6bdcd84e6
“rifam” <vinicius.rifam@gmail.com>
2020-02-06 10:53:06 -0300

    add Paginate and CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -6,6 +7,7 @@ const requireDir = require('require-dir');
 //iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //iniciando o DB
 
