6dbb207e0d0cde93bdd18eb528d509e18dd56f0d
“rifam” <vinicius.rifam@gmail.com>
2020-02-06 10:40:13 -0300

    CRUD

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,11 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get ('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
- module.exports = routes; 
+
+
+module.exports = routes; 
